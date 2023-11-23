284cbbd686b2173773e380ea74e4d1a08146e989
Gareth Gooch <gareth.gooch@education.gov.uk>
2023-11-23 14:42:32 +0000

    Azure App

---
+++ triggers/UploadRecord.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace gg_test_fa.triggers;
+
+public class UploadRecord
+{
+    public UploadRecord()
+    {
+        
+    }
+
+    [FunctionName("upload")]
+    
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "upload")] HttpRequest request)
+    {
+        Console.WriteLine(">>This has been hit");
+        return new ObjectResult("This was hit ok");
+    }
+}
